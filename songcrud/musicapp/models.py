d4a8676831d9a1f9928f79f9529950cd01e1b8ea
Sandra Igwilo <singng2@gmail.com>
2022-10-27 01:21:26 +0100

    @

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste (models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+
+class Song (models.Model):
+    title = models.CharField(max_length=100)
+    date_released = models.DateTimeField(default = datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+
+class Lyric (models.Model):
+    content = models.CharField(max_length=400)
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE) 
