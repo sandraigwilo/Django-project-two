1b60a13dc5d1fe0a350c97a259048ee5a1967492
Sandra Igwilo <singng2@gmail.com>
2022-10-28 05:13:23 +0100

    work on models.py, created models on database.

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -8,6 +8,9 @@ class Artiste (models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.first_name
+
 
 class Song (models.Model):
     title = models.CharField(max_length=100)
@@ -15,6 +18,11 @@ class Song (models.Model):
     likes = models.IntegerField()
     artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
 
+    def __str__(self):
+        return self.title
 class Lyric (models.Model):
     content = models.CharField(max_length=400)
     song_id = models.ForeignKey(Song, on_delete = models.CASCADE) 
+
+    def __str__(self):
+        return self.content
